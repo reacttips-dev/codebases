43c7c4db9505780a202df57d6db18b4513d72b74
Nguy Van Thang <nguy.van.thang@linecorp.com>
2021-09-23 10:39:16 +0700

    Remove redundancy

--- codebases/anchor.fm(1)/node_modules/react-with-styles-interface-css/dist/utils/constants.js
+++
@@ -1,8 +0,0 @@
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var GLOBAL_CACHE_KEY = 'reactWithStylesInterfaceCSS';
-var MAX_SPECIFICITY = 20;
-
-exports.GLOBAL_CACHE_KEY = GLOBAL_CACHE_KEY;
-exports.MAX_SPECIFICITY = MAX_SPECIFICITY;
