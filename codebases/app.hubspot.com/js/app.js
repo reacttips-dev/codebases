1add5512b75330239f6a96f279a35d78a0ec20f1
Thắng Nguỵ <bboy114crew@gmail.com>
2021-09-24 21:40:42 +0700

    Merge master

--- codebases/app.hubspot.com/js/app.js
+++ codebases/app.hubspot.com/js/app.js
@@ -1,122 +1,173 @@
-/* eslint no-new: 0 */
 'use es6';
 
 import _slicedToArray from "@babel/runtime/helpers/esm/slicedToArray";
-import _classCallCheck from "@babel/runtime/helpers/esm/classCallCheck";
-import _createClass from "@babel/runtime/helpers/esm/createClass";
+import { jsxs as _jsxs } from "react/jsx-runtime";
 import { jsx as _jsx } from "react/jsx-runtime";
-import i18n from 'I18n';
-import ReactDOM from 'react-dom';
-import { Router } from 'react-router';
-import { Provider } from 'react-redux';
-import { fromJS } from 'immutable';
-import userInfo from 'hub-http/userInfo';
-import PortalIdParser from 'PortalIdParser';
-import createHistory from 'SequencesUI/util/createHistory';
-import { hasSequencesAccess } from './lib/permissions';
-import configureStore from './util/configureStore';
-import { tracker } from './util/UsageTracker';
-import { RhumbProvider } from 'react-rhumb';
-import historyAdapter from 'react-rhumb/historyAdapterV3';
-import rhumbConfig from '../rhumb-config.yaml';
-import GateContainer from './data/GateContainer';
-import UserContainer from './data/UserContainer';
-import PortalContainer from './data/PortalContainer';
-import EmailConfirmBarPrebuilt from 'email-confirm-ui/prebuilts/HubHttpPrebuilt';
-import { DataFetchingClient, DataFetchingClientProvider } from 'data-fetching-client';
-import generateRoutes from './lib/routes';
-import initScopeContainer from './initializers/initScopeContainer';
-var GATES_TO_CHECK = ['Sequences:EnrollmentsReadOnlyView', 'Sequences:WootricSurveyEnabled', 'Sequences:WorkflowEnroll', 'Sequences:EmbeddedAutomation', 'sequences-settings-tab', 'Sequences:NewEmailPerformance'];
-var client = new DataFetchingClient();
+import { useEffect, useState, useContext, useCallback, useMemo, createContext } from 'react';
+import ContextualFlydown from './ContextualFlydown';
+import TrialBanner from './TrialBanner';
+import { FlydownContext } from './FlydownContext';
+import { postMessage, verifyEvent } from './utils';
+import { NavMarker } from 'react-rhumb';
+import { useMaybeAutoExpandFlydown } from './useMaybeAutoExpandFlydown';
+import { usePortalSettings } from './usePortalSettings';
+import SwitchTrialViewPopover from './SwitchTrialViewPopover';
+import { isAdmin } from 'ui-addon-upgrades/_core/common/eligibility/getIsAdmin';
+import { tracker, getCommonTrackingProperties, syncTracker } from './tracker';
+import { frameProps, isTrialActive } from './queryParamUtils';
+import { MultiTrialDropdownContext } from './contexts/MultiTrialDropdownContext';
+import { useUserInfo } from './useUserInfo';
+import { useForwardSalesChatEvent } from './utils/zorseWidgetUtils';
+import AlertWrapper from './utils/AlertWrapper';
+import { useIsUngated } from './useIsUngated';
+export var TrialStateContext = /*#__PURE__*/createContext();
 
-var Application = /*#__PURE__*/function () {
-  function Application() {
-    _classCallCheck(this, Application);
-  }
+function App() {
+  var _useContext = useContext(FlydownContext),
+      showFlydown = _useContext.showFlydown,
+      setShowFlydown = _useContext.setShowFlydown;
 
-  _createClass(Application, [{
-    key: "start",
-    value: function start() {
-      window.AppInstance = this;
+  var _useContext2 = useContext(MultiTrialDropdownContext),
+      setShowMultiTrialDropdown = _useContext2.setShowMultiTrialDropdown,
+      setSwitchTrialViewRef = _useContext2.setSwitchTrialViewRef,
+      switchTrialViewRef = _useContext2.switchTrialViewRef;
 
-      if (window.newrelic && window.performance && window.performance.getEntriesByName) {
-        var scriptStartEntries = window.performance.getEntriesByName('scriptStartTime');
+  var _useState = useState(frameProps.trialBannerState),
+      _useState2 = _slicedToArray(_useState, 2),
+      trialState = _useState2[0],
+      setTrialState = _useState2[1];
 
-        if (scriptStartEntries.length > 0) {
-          window.newrelic.setCustomAttribute('scriptStartTime', scriptStartEntries[0].startTime);
-          window.newrelic.setCustomAttribute('appStartTime', window.performance.now());
+  var _useState3 = useState(frameProps.preferredTrial || {}),
+      _useState4 = _slicedToArray(_useState3, 2),
+      preferredTrial = _useState4[0],
+      setPreferredTrial = _useState4[1];
+
+  var _useState5 = useState(false),
+      _useState6 = _slicedToArray(_useState5, 2),
+      hasTrackedPageView = _useState6[0],
+      setHasTrackedPageView = _useState6[1];
+
+  useEffect(function () {
+    window.addEventListener('message', function (event) {
+      if (verifyEvent(window.parent, event)) {
+        if (event.data === 'FLYDOWN_CLOSE') {
+          postMessage('FLYDOWN_CLOSE');
+          setShowFlydown(false);
+          setShowMultiTrialDropdown(false); // Dismiss MultiTrialDropdown when user clicks outside of banner iframe
+
+          setSwitchTrialViewRef(null); // Dismiss "switch trial views" popover when user clicks outside of banner iframe
         }
       }
+    });
+  }, [setShowFlydown, switchTrialViewRef, setSwitchTrialViewRef, setShowMultiTrialDropdown]); /// for tts chat event capture
 
-      this.init().then(this.actualStart.bind(this)).done();
+  useForwardSalesChatEvent();
+  var productApiName = preferredTrial.productApiName,
+      expiresAt = preferredTrial.expiresAt,
+      upgradeProduct = preferredTrial.trialName;
+  var activeTrials = useMemo(function () {
+    if (!trialState) {
+      return [];
     }
-  }, {
-    key: "init",
-    value: function init() {
-      PortalIdParser.get();
-      return Promise.all([userInfo(), i18n.Info]).then(function (_ref) {
-        var _ref2 = _slicedToArray(_ref, 1),
-            authData = _ref2[0];
 
-        var user = authData.user,
-            gates = authData.gates,
-            portal = authData.portal;
-        UserContainer.set(user);
-        PortalContainer.set(fromJS(portal));
-        initScopeContainer(user.scopes);
-        GateContainer.set(GATES_TO_CHECK.reduce(function (acc, gate) {
-          acc[gate] = gates.includes(gate);
-          return acc;
-        }, {}));
+    return trialState.filter(function (trial) {
+      return isTrialActive(trial.status);
+    });
+  }, [trialState]);
+  var expiredTrials = useMemo(function () {
+    if (!trialState) {
+      return [];
+    }
 
-        if (!hasSequencesAccess()) {
-          return window.location.href = "/upgrade/" + PortalIdParser.get() + "/sequences";
-        }
+    return trialState.filter(function (trial) {
+      return !isTrialActive(trial.status);
+    });
+  }, [trialState]);
+  var isMultiTrial = activeTrials.length > 1;
+  var userInfo = useUserInfo();
+  var isUngatedForBannerPortals = useIsUngated('Trials:BannerPortals');
+  useEffect(function () {
+    if (productApiName && expiresAt && upgradeProduct && isMultiTrial !== null) {
+      tracker.setProperties(getCommonTrackingProperties({
+        activeTrials: activeTrials,
+        apiName: productApiName,
+        expiredTrials: expiredTrials,
+        expiresAt: expiresAt,
+        upgradeProduct: upgradeProduct,
+        isMultiTrial: isMultiTrial
+      }));
+      syncTracker.setProperties(getCommonTrackingProperties({
+        activeTrials: activeTrials,
+        apiName: productApiName,
+        expiredTrials: expiredTrials,
+        expiresAt: expiresAt,
+        upgradeProduct: upgradeProduct,
+        isMultiTrial: isMultiTrial
+      }));
+    }
 
-        return {
-          authData: authData
-        };
+    if (!hasTrackedPageView && trialState && userInfo) {
+      // de-abstract isAdmin call so we can track whether the data has been fetched
+      var userIsAdmin = !!(userInfo && isAdmin(userInfo));
+      tracker.setProperties({
+        isAdmin: userIsAdmin
       });
+      tracker.track('pageView');
+      postMessage('FRAME_READY');
+      setHasTrackedPageView(true);
     }
-  }, {
-    key: "actualStart",
-    value: function actualStart() {
-      tracker.track('pageView', {
-        subscreen: 'sequences-index'
-      });
-      this.startRedux();
-      new EmailConfirmBarPrebuilt();
-    }
-  }, {
-    key: "startRedux",
-    value: function startRedux() {
-      this.store = configureStore();
-      this.initRouter();
-    }
-  }, {
-    key: "initRouter",
-    value: function initRouter() {
-      var pageContainer = document.getElementsByClassName('page')[0];
-      var history = createHistory("/sequences/" + PortalIdParser.get());
-      ReactDOM.render( /*#__PURE__*/_jsx(RhumbProvider, {
-        captureExceptions: true,
-        config: rhumbConfig,
-        history: historyAdapter(history),
-        children: /*#__PURE__*/_jsx(Provider, {
-          store: this.store,
-          children: /*#__PURE__*/_jsx(DataFetchingClientProvider, {
-            client: client,
-            children: /*#__PURE__*/_jsx(Router, {
-              routes: generateRoutes(),
-              history: history
-            })
-          })
-        })
-      }), pageContainer);
-    }
-  }]);
+  }, [productApiName, expiresAt, upgradeProduct, isMultiTrial, activeTrials, expiredTrials, hasTrackedPageView, trialState, userInfo]);
+  var portalSettings = usePortalSettings(showFlydown);
+  var handleShowFlydownClick = useCallback(function () {
+    setShowMultiTrialDropdown(false);
 
-  return Application;
-}();
+    if (!showFlydown) {
+      postMessage('FLYDOWN_OPEN');
+      setShowFlydown(true);
+    } else {
+      setShowFlydown(false);
+      postMessage('FLYDOWN_CLOSE');
+    }
+  }, [showFlydown, setShowFlydown, setShowMultiTrialDropdown]);
 
-export { Application as default };
+  var renderFlydown = function renderFlydown() {
+    if (!showFlydown) return null;
+    return /*#__PURE__*/_jsx(ContextualFlydown, {
+      preferredTrialUpgradeProduct: upgradeProduct,
+      onToggleFlydown: handleShowFlydownClick
+    });
+  };
+
+  useMaybeAutoExpandFlydown(portalSettings, handleShowFlydownClick); // Some flows require the flydown to be open upon first visit
+
+  var hasFetchedAllNeededInfoForTracking = !!(upgradeProduct && userInfo);
+  var isLoadingGate = isUngatedForBannerPortals === null;
+
+  if (!hasFetchedAllNeededInfoForTracking || isLoadingGate) {
+    return null;
+  }
+
+  return /*#__PURE__*/_jsxs("div", {
+    "data-test-id": "trial-banner",
+    children: [/*#__PURE__*/_jsx(NavMarker, {
+      name: "BANNER_LOAD"
+    }), /*#__PURE__*/_jsx(AlertWrapper, {
+      children: /*#__PURE__*/_jsxs(TrialStateContext.Provider, {
+        value: {
+          trialState: trialState,
+          onChangeTrialState: setTrialState,
+          preferredTrial: preferredTrial,
+          onChangePreferredTrial: setPreferredTrial
+        },
+        children: [/*#__PURE__*/_jsx(TrialBanner, {
+          onShowFlydownClick: handleShowFlydownClick,
+          showFlydown: showFlydown
+        }), renderFlydown(), /*#__PURE__*/_jsx(SwitchTrialViewPopover, {
+          showFlydown: showFlydown
+        })]
+      })
+    })]
+  });
+}
+
+export default App;
